eb9e64683466633ad5310f15c394877924b5cfb3
binze <binze.80@gmail.com>
2020-08-11 19:16:38 +0530

    second commit

--- readwriteExcel_openpyxl.py
+++ readwriteExcel_openpyxl.py
@@ -2,7 +2,7 @@ import openpyxl
 
 
 def copy_paste():
-    wb_read = openpyxl.load_workbook(r"C:\Users\z003tcpp\Downloads\Binu_CurrentHoldings.xlsx")
+    wb_read = openpyxl.load_workbook(r"D:\Chrome Downloads\Binu_CurrentHoldings.xlsx")
 
     sheet_read = wb_read.active
     #print(f'\nsheet active in Fundsindia is  {sheet_read}')
@@ -14,22 +14,26 @@ def copy_paste():
     #sheet_write = sheet.active
     #print(f'Sheet active in Bank is {sheet_write}')
 
+	#Reading the invested value from the downloaded excel
     value_invested = []
     for i in range(3, 10):
         value_invested.append(sheet_read.cell(row=i, column=5).value)
 
+	#Writing invested values into Bank excel
     x = 0
-    for j in range(28, 35):
+    for j in range(32, 39):
         write_value = sheet_write.cell(row=j, column=6)
         write_value.value = value_invested[x]
         x = x + 1
 
+	#Reading the actual value from the downloaded excel
     value_return = []
     for i in range(3, 10):
         value_return.append(sheet_read.cell(row=i, column=11).value)
 
+	#Writing actual values into Bank excel
     x = 0
-    for j in range(28, 35):
+    for j in range(32, 39):
         write_value = sheet_write.cell(row=j, column=7)
         write_value.value = value_return[x]
         x = x + 1
@@ -43,12 +47,9 @@ def copy_paste():
     #print(f'Gain is        = ₹ {round(G, 2)}')
     wb_write.save("H:\Own\Bank.xlsx")
     if R > I:
-        print(f"Profit        = ₹{round(G, 2)}",)
+        print(f"Profit         = ₹{round(G, 2)}",)
     else:
         print(f"Loss😔          = ₹{round(G, 2)}")
 
 
 copy_paste()
-
-
-
